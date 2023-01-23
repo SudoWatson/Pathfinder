1676e501b2e85cfbe46d571105ab31a5e809b57f
SudoWatson <aulennert@gmail.com>
2023-01-22 19:47:19 -0600

    Initial commit

---
+++ src/algorithims/astar.ts
@@ -0,0 +1,236 @@
+/** Cost for a node to go straight to another node */
+const STRAIGHT_COST = 10;
+/** Cost for a node to go diagonal to another node */
+const DIAGONAL_COST = 14;
+
+// type Node = {
+//     /** Node position on grid */
+//     pos: p5.Vector,
+//     /** Distance from starting node */
+//     gCost: number,
+//     /** Distance from end node */
+//     hCost: number
+// }
+
+// TODO Each algorithm gets its own folder, with a meta file
+// TODO Export this to own file
+class Node {
+    /** Node position on grid */
+    public pos: p5.Vector;
+    /** Distance from starting node */
+    public gCost: number;
+    /** Hypothetical distance from end node */
+    public hCost: number;
+    /** GCost + HCost; Total expected distance along path this node is part of */
+    //public fCost: number;
+    /** Parent node along current shortest path */
+    public parentNode: Node | null;
+
+    constructor(pos: p5.Vector, gCost: number) {
+        this.pos = pos;
+        this.gCost = gCost;
+        this.hCost = 0;
+        this.parentNode = null;
+    }
+
+    /** GCost + HCost: Total expected distance along path this node is part of */
+    get fCost() {
+        return this.gCost + this.hCost;
+    }
+
+    /** Update the hCost according the the given end node */
+    updateHCost(endNode: Node) {
+        this.hCost = distanceBetween(this.pos, endNode.pos);
+    }
+}
+
+class AStarSolver {
+
+    /** p5 instance */
+    private p: p5;
+    /** Nodes already searched and found best path to; Closed list */
+    private searched: Node[];
+    /** Nodes to search and find best path to; Open list */
+    private toSearch: Node[];
+    private grid: String[][];
+    private startNode: Node;
+    private endNode: Node;
+
+    constructor(p: p5, grid: string[][], startPos: p5.Vector, endPos: p5.Vector) {
+        this.searched = [];
+        this.toSearch = [];
+        this.grid = grid;
+        this.p = p;
+        this.endNode = new Node(endPos, 0);
+        this.startNode = new Node(startPos, 0);
+        this.startNode.updateHCost(this.endNode);
+        this.toSearch.push(this.startNode);
+    }
+
+    stepGrid() {
+        /** The node with the lowest fCost currently */
+        let currentNode: Node = this.toSearch[0];
+        if (this.toSearch.length === 0) {
+            // No nodes left to search, impossible to solve
+            console.log("Cannot find a path");
+            return;
+        }
+        // Get closest open node to end
+        this.toSearch.forEach((newNode: Node) => {
+            // If currentNode already has lower fCost, don't bother
+            if (currentNode.fCost < newNode.fCost) return;  // Exit forEach
+            // If fCosts are equal, go by hCost
+            if (currentNode.hCost < newNode.hCost) return;
+            // Otherwise, this new node is better
+            currentNode = newNode;
+        })
+
+        // Remove currentNode from toSearch list
+        let indexOfCurrentNode = this.toSearch.indexOf(currentNode);
+        if (indexOfCurrentNode === -1) {
+            console.error("Could not find currentNode in open list");
+            return;
+        }
+        this.toSearch.splice(indexOfCurrentNode, 1);
+        this.addToSearched(currentNode);
+        if (currentNode.pos.equals(this.endNode.pos)) {
+            // Finished
+            console.log("Found shortest path");
+            
+            return;
+        }
+
+        let cnx = currentNode.pos.x;
+        let cny = currentNode.pos.y;
+        for (let neighborX = cnx-1; neighborX <= cnx+1; neighborX++) {
+
+            // Out of bounds, don't check
+            if (neighborX <= -1) continue;
+            if (neighborX >= this.grid.length) continue;
+
+            for (let neighborY = cny-1; neighborY <= cny+1; neighborY++) {
+
+                // Out of bounds, don't check
+                if (neighborY <= -1) continue;
+                if (neighborY >= this.grid[0].length) continue;
+
+                let neighborPos = this.p.createVector(neighborX, neighborY);
+
+
+                // Check if neighbor is in closed list
+                let inClosed = false;
+                for (const searchedNode of this.searched) {
+                    if (searchedNode.pos.equals(neighborPos)) {
+                        inClosed = true;
+                        break;
+                    }
+                }
+                // If neighbor is in closed group or is a barrier, skip to next
+                if (inClosed || this.grid[neighborX][neighborY] === "B") continue;
+                
+
+                // Check if neighbor is in open list
+                let inOpen = false;
+                let neighborNode: Node;
+                for (const nodeToSearch of this.toSearch) {
+                    if (nodeToSearch.pos.equals(neighborPos)) {
+                        inOpen = true;
+                        neighborNode = nodeToSearch;
+                        break;
+                    }
+                }
+
+
+                /** Boolean of whether the new nodes fCost is close with the currentNode as parent or not */
+                let isCloserWithThisNode = false;
+                    
+                // Boolean of whether the new node is inline with an axis of the currentNode
+                let isInline = neighborX === currentNode.pos.x || neighborY === currentNode.pos.y;
+
+                let potentialNewGCost = currentNode.gCost + ((isInline) ? STRAIGHT_COST : DIAGONAL_COST);
+                if (inOpen) {
+                    isCloserWithThisNode = potentialNewGCost < neighborNode!.gCost;
+                }
+
+                // if neighbor is not in toSearch list, or neighbor is close with currentNode as parent
+                if (!inOpen || isCloserWithThisNode) {
+                    if (!inOpen) {  // Node doesn't yet exist, create it
+                        neighborNode = new Node(neighborPos, potentialNewGCost);
+                        neighborNode.updateHCost(this.endNode);
+                        this.addToToSearched(neighborNode);
+                    }
+                    neighborNode!.parentNode = currentNode;
+                    neighborNode!.gCost = potentialNewGCost;
+                }
+            }
+        }
+
+        /*
+        
+        foreach neighbor of currentNode:
+            if neighbor is barrier OR neighbor is already searched:
+                move on to next neighbor
+            if fCost of neighbor is shorter with current, or neighbor is not in toSearch list:
+                update the fCost of neighbor
+                set parent of neighbor to currentNode
+                add neighbor to toSearch
+        */
+    }
+
+    /** Renders the node values as an overlay */
+    renderNodeValues(squareSize: number) {
+        for (const node of [...this.toSearch, ...this.searched]) {
+            let p = this.p;
+            p.push();
+            p.translate(node.pos.x*squareSize + squareSize/2, node.pos.y*squareSize + squareSize / 2);
+
+            p.fill(255);
+            p.textAlign(p.CENTER);
+            p.text(node.fCost, 0, 0);
+            p.text(node.gCost, -10, -10);
+            p.text(node.hCost, 10, -10);
+
+            p.pop();
+        }
+    }
+
+    get squaresSearched() {
+        return [...this.searched];
+    }
+
+    get squaresToSearch() {
+        return [...this.toSearch];
+    }
+
+    /** Add the node to the 'toSearched' list */
+    addToToSearched(node: Node) {
+        this.grid[node.pos.x][node.pos.y] = 'O';
+        this.toSearch.push(node);
+    }
+
+    /** Add the node to the 'searched' list */
+    addToSearched(node: Node) {
+        this.grid[node.pos.x][node.pos.y] = 'C';
+        this.searched.push(node);
+    }
+}
+
+export default AStarSolver;
+
+/** Returns the heuristic distance from Vector to Vector */
+function distanceBetween(pos1: p5.Vector, pos2: p5.Vector): number {
+    let distance = 0;
+    // Get the axis locked distances
+    let dx = Math.abs((pos1.x-pos2.x));
+    let dy = Math.abs((pos1.y-pos2.y));
+    
+    // The smallest axis distance is the amount of STRAIGHT steps to go
+    if (dx < dy) distance += dx * DIAGONAL_COST;
+    else distance += dy * DIAGONAL_COST;
+
+    // The difference of the axis distance is the amount of DIAGONAL steps to go
+    distance += Math.abs(dx - dy) * STRAIGHT_COST;
+
+    return distance;
+}
+
---
+++ src/main.ts
@@ -0,0 +1,166 @@
+import { __Graphics__ } from "p5";
+import AStarSolver from "./algorithims/astar.js";
+
+
+let squareSize = 50;
+
+// Amount of columns/rows on the grid
+let cols: number, rows: number;
+
+// The starting and ending node for the algorithim
+let startPos: p5.Vector, endPos: p5.Vector;
+
+// Array of barrier positions
+let barriers = [];
+
+
+
+// Width and Height of the canvas
+let WIDTH: number, HEIGHT: number;
+
+/** Array grid containing all of the squares information. grid[x][y] */ 
+let grid: string[][];
+
+let aStar: AStarSolver;
+
+let i = 0;
+
+const sketch = (p: p5) => {
+    p.setup = () => {
+        // ---------------- P5 SETUP ---------------- \\
+       
+        // Calculate how many squares can fit on screen
+        cols = Math.floor((p.windowWidth*0.99)/squareSize);
+        rows = Math.floor((p.windowHeight/2)/squareSize);
+
+        // Canvas Dimensions
+        WIDTH = Math.floor(cols * squareSize);
+        HEIGHT = Math.floor(rows * squareSize);
+
+        p.frameRate(1);
+
+        // Create p canvas
+        let canvas = p.createCanvas(WIDTH, HEIGHT);
+
+        // Disable right-click context menu on grid
+        canvas.elt.addEventListener("contextmenu", (e: any) => e.preventDefault())
+
+
+        
+        grid = createEmptyGrid(rows, cols);
+        // ---------------- END P5 SETUP ---------------- \\
+
+        let resetButton = p.createButton("Reset");
+        resetButton.mouseClicked(reset);
+
+
+        // TODO START TEMPORARY
+reset();
+        // TODO End Temporary
+    }
+
+    p.draw = () => {
+        p.background(220);
+
+        // Loop through grid and draw squares
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                let type = grid[i][j];
+                // Set color depending on fill type
+                if (type === ' ') p.fill(255);
+                else if (type === 'B') p.fill(0);
+                else if (type === 'S') p.fill(230,0,0);
+                else if (type === 'E') p.fill(0,160,0);
+                else if (type === 'C') p.fill(0,160,160);
+                else if (type === 'O') p.fill(0,0,255);
+                p.square(i * squareSize, j * squareSize, squareSize);
+            }
+        }
+        aStar.stepGrid();
+        aStar.renderNodeValues(squareSize);
+    }
+
+    // Places a barrier
+    p.mousePressed = () => {place()}
+    p.mouseDragged = () => {place()}
+
+    p.keyPressed = () => {
+        // aStar.stepGrid();
+    }
+
+    /**
+     * Places a barrier at the mouse position if left click
+     * Removes a barrier at the mouse position if right click
+     */
+    function place(): void {
+        // Position to set
+        let position = p.createVector(Math.floor(p.mouseX/squareSize),Math.floor(p.mouseY/squareSize));
+        
+        // TODO Another invalid position when going left of grid, non-fetal
+        // Invalid positions
+        if (position.x >= cols) return;
+        if (position.y >= rows) return;
+        if (position.equals(startPos) || position.equals(endPos)) return;
+
+        // Set with left, clear with right click
+        if (p.mouseButton == "left") grid[position.x][position.y] = 'B';
+        else if (p.mouseButton == "right") grid[position.x][position.y] = ' ';
+    }
+
+    /**
+     * Sets a position in a grid to a given block
+     * @param {Vector} pos - Position to set
+     * @param {String} setTo - Char string to set the position to
+     */
+    function setPos(pos: p5.Vector, setTo: string): void {
+        grid[pos.x][pos.y] = setTo;
+    }
+
+    /**
+     * Creates an empty grid of given rows x cols
+     * @param {number} rows 
+     * @param {number} cols 
+     * @returns {string[][]} Empty grid
+     */
+    function createEmptyGrid(rows: number, cols: number): string[][] {
+        let grid = [];
+        for (let i = 0; i < cols; i++) {
+            let innerArray = [];
+            for (let j = 0; j < rows; j++) {
+                innerArray[j] = ' ';
+            }
+            grid[i] = innerArray;
+        }
+        return grid;
+    }
+
+    function reset() {
+        // Set start and end points
+        startPos = p.createVector(6,6);
+        endPos = p.createVector(3,3);
+        setPos(startPos, 'S');
+        setPos(endPos, 'E');
+
+        // TODO this feels bad
+        for (const rowi in grid) {
+            if (Object.prototype.hasOwnProperty.call(grid, rowi)) {
+                const row = grid[rowi];
+                for (const squarei in row) {
+                    if (Object.prototype.hasOwnProperty.call(row, squarei)) {
+                        const square = row[squarei];
+                        if (square !== 'B' && square !== 'S' && square !== 'E') {
+                            grid[rowi][squarei] = ' ';
+                        }
+                    }
+                }
+            }
+        }
+
+
+        aStar = new AStarSolver(p, grid, startPos, endPos);
+    }
+}
+
+// Typescript is complaining about stupid shit it doesn't need to complain about
+// @ts-ignore
+new p5(sketch);
